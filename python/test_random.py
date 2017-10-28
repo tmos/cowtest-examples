aaa44e3b621fc98939c1eb267bc2857db293c47f
Tom Canac <contact@tomcanac.com>
2017-10-28 15:42:19 -0400

    Proper test

--- python/test_random.py
+++ python/test_random.py
@@ -1,10 +1,18 @@
 import unittest
 import os
+from selenium import webdriver
+driver = webdriver.PhantomJS()
 
 class UX_tests(unittest.TestCase):
     def test_homepageTitle(self):
-         assert os.environ.get('TEST_URL') == 'True';
+        driver.get(os.environ.get('TEST_URL'))
+        pageTitle = driver.title
+        driver.quit()
 
+        assert pageTitle != '';
+
+
+"The next bloc makes the test output TAP"
 if __name__ == '__main__':
     from pycotap import TAPTestRunner
     suite = unittest.TestLoader().loadTestsFromTestCase(UX_tests)
