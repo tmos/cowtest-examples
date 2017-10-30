e1ca7ea0ca073b94c4fe46729e0f0c096d5055bf
Tom Canac <contact@tomcanac.com>
2017-10-30 15:35:00 -0400

    Better example tests

--- nodejs/avaTest.js
+++ nodejs/avaTest.js
@@ -1,7 +1,10 @@
 import test from 'ava';
 import Nightmare from 'nightmare';
+import w3cjs from 'w3cjs';
 
-test('Console shouldn\'t output error or warning logs', async (t) => {
+const URL = process.env.TEST_URL;
+
+test('Console shouldn\'t output error logs', async (t) => {
   const nightmare = Nightmare();
   const errors = [];
   const warnings = [];
@@ -14,9 +17,56 @@ test('Console shouldn\'t output error or warning logs', async (t) => {
       if (type==='warn')
         warnings.push(message);
       })
-    .goto(process.env.TEST_URL)
+    .goto(URL)
     .end();
 
-  t.is(errors.length, 0);
-  t.is(warnings.length, 0);
+    if (errors.length > 0) {
+      t.fail(errors)
+    } else {
+      t.pass();
+    }
+});
+
+test('Console shouldn\'t output warning logs', async (t) => {
+  const nightmare = Nightmare();
+  const errors = [];
+  const warnings = [];
+
+  await nightmare
+    .on('console', (type, message) => {
+      if (type==='error')
+        errors.push(message);
+
+      if (type==='warn')
+        warnings.push(message);
+      })
+    .goto(URL)
+    .end();
+
+  if (warnings.length > 0) {
+    t.fail(warnings)
+  } else {
+    t.pass();
+  }
+});
+
+test.serial('Should be validated by the W3C', async (t) => {
+  function validate(validateUrl) {
+    return new Promise(function(resolve, reject) {
+      w3cjs.validate({
+        file: validateUrl,
+        output: 'json',
+        callback: (err, res) => {
+          resolve(res);
+        }
+      });
+    });
+  }
+
+  const res = await validate(URL);
+  if (res.messages.length > 0) {
+    t.fail(res.messages)
+  } else {
+    t.pass();
+  }
 });
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,6 @@
 const Cowtest = require('./../../cowtest/dist/').default;
 
-const seedUrl = 'http://127.0.0.1:8080';
+const seedUrl = 'http://127.0.0.1:8080/';
 const tests = `${__dirname}/avatest.js`;
 const connector = 'ava';
 const reporter = 'html';
