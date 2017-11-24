dadd3eb83233da4f2a3cf388f11cab6610570a15
Tom Canac <contact@tomcanac.com>
2017-11-24 16:10:27 -0500

    👌

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,15 @@
-const Cowtest = require('./../../cowtest/dist/').default;
+const Cowtest = require("./../../cowtest/dist/").default;
 
-const seedUrl = 'http://127.0.0.1:8080/';
+const seedUrl = "http://127.0.0.1:8080/";
 const tests = `${__dirname}/avaTest.js`;
-const connector = 'ava';
-const reporter = 'html';
+const connector = "ava";
+const reporter = "html";
+const dataSaveMethod = { method: 'jsonl', coStr: __dirname + '/data.jsonl' }
 
-Cowtest(
+Cowtest({
   seedUrl,
   tests,
   connector,
-  reporter,
-);
+  dataSaveMethod,
+  reporter
+});
